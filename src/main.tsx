edc02473c5c891e942371a6ce138d68337529e28
GabrielAgrela <gabriel.agrela@hotmail.com>
2024-01-06 12:29:45 -0300

    feat: Created browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import './index.css'
+import { Home } from './domain/home/Home.tsx'
+import { Wallet } from './domain/wallet/Wallet.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/wallet",
+    element: <Wallet />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
